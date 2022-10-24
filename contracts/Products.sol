39504384dc81f510243cdcf76649718f7efde579
anarkrypto <anarkrypto@gmail.com>
2022-10-24 07:37:41 -0300

    allow add & remove operators, add events info

--- contracts/Products.sol
+++ contracts/Products.sol
@@ -30,8 +30,19 @@ contract Products is Suppliers {
         uint256 productId,
         uint256 supplierId,
         address holderAddr,
-        string metadataURI
+        bool holderIsContract,
+        string metadataURI,
+        address manager
     );
+
+    event NewOperator(uint256 productId, address operator, address manager);
+
+    event RemovedOperator(uint256 productId, address operator, address manager);
+
+    modifier productExists(uint256 productId) {
+        require(_products[productId].supplierId != 0, "Product does not exist");
+        _;
+    }
 
     // Register product information
     function addProduct(
@@ -49,13 +60,25 @@ contract Products is Suppliers {
                 !_holderContracts[holderAddr],
                 "Holder contract already exists"
             );
-            _holderContracts[holderAddr] = true;
             _doSafeHolderFullCheck(holderAddr);
+            _holderContracts[holderAddr] = true;
         }
         _numberOfProducts.increment();
         uint256 productId = _numberOfProducts.current();
-        _products[productId] = Product(supplierId, holderAddr, holderIsContract, metadataURI);
-        emit NewProduct(productId, supplierId, holderAddr, metadataURI);
+        _products[productId] = Product(
+            supplierId,
+            holderAddr,
+            holderIsContract,
+            metadataURI
+        );
+        emit NewProduct(
+            productId,
+            supplierId,
+            holderAddr,
+            holderIsContract,
+            metadataURI,
+            msg.sender
+        );
 
         // Grant token transfer rights to manager
         if (msg.sender != holderAddr) {
@@ -64,24 +87,31 @@ contract Products is Suppliers {
     }
 
     // Get product information by id
-    function getProduct(uint256 id) public view returns (Product memory) {
+    function getProduct(uint256 id)
+        public
+        view
+        productExists(id)
+        returns (Product memory)
+    {
         return (_products[id]);
     }
 
-    function getSupplierIdFromProduct(uint256 productId)
+    function getSupplierIdFromProduct(uint256 id)
         public
         view
-        returns (uint256 supplierId)
+        productExists(id)
+        returns (uint256)
     {
-        supplierId = _products[productId].supplierId;
+        return _products[id].supplierId;
     }
 
-    function getProductHolder(uint256 productId)
+    function getProductHolder(uint256 id)
         public
         view
-        returns (address holderAddress)
+        productExists(id)
+        returns (address)
     {
-        holderAddress = _products[productId].holderAddr;
+        return _products[id].holderAddr;
     }
 
     function getSupplierProducts(uint256 supplierId)
@@ -100,10 +130,39 @@ contract Products is Suppliers {
         return products;
     }
 
+    function addOperator(uint256 productId, address operator)
+        public
+        productExists(productId)
+        allowedSupplier(_products[productId].supplierId)
+        allowedManager(_products[productId].supplierId, msg.sender)
+    {
+        require(
+            _products[productId].holderIsContract,
+            "Product holder is not a contract"
+        );
+        BatchesOperator(_products[productId].holderAddr).addOperator(operator);
+        emit NewOperator(productId, operator, msg.sender);
+    }
+
+    function removeOperator(uint256 productId, address operator)
+        public
+        productExists(productId)
+        allowedManager(_products[productId].supplierId, msg.sender)
+    {
+        require(
+            _products[productId].holderIsContract,
+            "Product holder is not a contract"
+        );
+        BatchesOperator(_products[productId].holderAddr).removeOperator(
+            operator
+        );
+        emit RemovedOperator(productId, operator, msg.sender);
+    }
+
     /**
      * Check if the contract implements BatchesHolder interface
      */
-    function _IBatchesHolderSupportCheck(address account) internal view {
+    function _IBatchesHolderSupportCheck(address account) private view {
         try
             BatchesHolder(account).supportsInterface(
                 type(IBatchesHolder).interfaceId
@@ -121,7 +180,7 @@ contract Products is Suppliers {
         }
     }
 
-    function _holderSupplierCheck(address account) public view {
+    function _holderSupplierCheck(address account) private view {
         // External call to contract asking for the supplier address
         (bool success, bytes memory data) = account.staticcall(
             abi.encodeWithSignature("getSupplier()")
@@ -137,7 +196,7 @@ contract Products is Suppliers {
     /**
      * Check if the contract implements BatchesHolder interface
      */
-    function _IBatchesOperatorSupportCheck(address account) internal view {
+    function _IBatchesOperatorSupportCheck(address account) private view {
         try
             BatchesOperator(account).supportsInterface(
                 type(IBatchesOperator).interfaceId
